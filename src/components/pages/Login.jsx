4a6ded264ef58ecb1c524d4659fce1af4627d99c
Emilse Arias <emilsearias@gmail.com>
2025-06-30 21:32:01 -0300

    cambie el titulo del proyecto

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,9 @@
 import { Row, Col, Form, Button } from "react-bootstrap";
 
 const Login = () => {
-  return (
 
-    
+  console.log(import.meta.env.VITE_API_EMAIL)  
+  return (   
     <section className="container my-3">
       <h1 className="text-center">Login</h1>
       <Row xs={1} md={2}>
