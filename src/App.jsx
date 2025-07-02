91cb812e497e69436c4c65bdfe9938752f52d262
Emilse Arias <emilsearias@gmail.com>
2025-07-02 20:31:14 -0300

    agrgeamos validaciones al formulario de login y nos logueamos como administradores

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ import CardProducto from "./components/pages/producto/CardProducto";
 import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
+import { useState } from "react";
 
 function App() {
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem('userKey')) || false;
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado)
+
   return (
     <>
       <BrowserRouter>
@@ -23,7 +27,7 @@ function App() {
             ></Route>
             <Route
               path="/login"
-              element={<Login></Login>}
+              element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}
             ></Route>
             <Route
               path="/administrador"
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,26 +1,59 @@
 import { Row, Col, Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
-const Login = () => {
+const Login = ({setUsuarioAdmin}) => {
+   const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const navegacion = useNavigate()
 
-  console.log(import.meta.env.VITE_API_EMAIL)  
+  const iniciarSesion = (usuario)=>{
+  console.log(usuario)
+  if(usuario.email === import.meta.env.VITE_API_EMAIL && usuario.password === import.meta.env.VITE_API_PASSWORD){
+    //soy el administrador
+    console.log('Soy el administrador')
+    setUsuarioAdmin(true)
+    sessionStorage.setItem('userKey', true)
+    navegacion('/administrador')
+  }else{
+    console.log('Email o contraseña incorrecto.')
+  }
+  }
+
+
   return (   
     <section className="container my-3">
       <h1 className="text-center">Login</h1>
       <Row xs={1} md={2}>
         <Col>
-          <Form>
+          <Form onSubmit={handleSubmit(iniciarSesion)}>
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Ej: juanperez@mail.com" />
+              <Form.Control type="email" placeholder="Ej: juanperez@mail.com" {...register('email',{
+                required: 'El email es un dato obligatorio',
+                pattern:{
+                  value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                  message: 'El email debe tener un formato valido, por ej: juanperez@mail.com'
+                }
+              })} />
               <Form.Text className="text-danger">
-                este es un mensaje de error
+               {errors.email?.message}
               </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="password">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
+              <Form.Control type="password" placeholder="Contraseña" {...register('password', {
+                required: 'La contraseña es un dato ogligatorio',
+                pattern:{
+                  value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                  message: 'La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.'
+                }
+              })}/>
                <Form.Text className="text-danger">
-                este es un mensaje de error
+                  {errors.password?.message}
               </Form.Text>
             </Form.Group>
             <Button variant="success" type="submit">
