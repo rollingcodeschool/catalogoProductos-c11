2ecd541d9d37a27f105cd587ee5cf34f4babe2f1
Emilse Arias <emilsearias@gmail.com>
2025-07-14 20:49:35 -0300

    instalamos uuid y sweetalert, ademas agregamos las validaciones del formulario de producto

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,6 +1,7 @@
 import { Button, Table } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
 import { productosData } from "../../data/productosPrueba";
+import { Link } from "react-router";
 
 const Administrador = ({ setProductos, productos }) => {
 
@@ -13,9 +14,9 @@ const Administrador = ({ setProductos, productos }) => {
       <div className="d-flex justify-content-between align-items-center mt-5">
         <h1 className="display-4 ">Productos disponibles</h1>
         <div>
-          <Button className="btn btn-primary">
+          <Link className="btn btn-primary" to={'/administrador/crear'}>
             <i className="bi bi-file-earmark-plus"></i>
-          </Button>
+          </Link>
           <Button className="btn btn-info ms-2 text-light" onClick={cargarProductosPrueba}>
             <i className="bi bi-database-fill-add"></i>
           </Button>
