7880ddedb423c64fbae077e4c84bafbd1527e026
Emilse Arias <emilsearias@gmail.com>
2025-08-20 21:14:59 -0300

    agregamos la variable de entorno del backend de productos y agregue el archivo de queries

---
+++ src/helpers/queries.js
@@ -0,0 +1,67 @@
+const urlproductos = import.meta.env.VITE_API_PRODUCTOS;
+// get, post, put, delete
+
+console.log(urlproductos);
+
+export const leerProductos = async () => {
+    try{
+        const respuesta = await fetch(urlproductos)
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
+export const obtenerProductoPorID = async (id) => {
+    try{
+        const respuesta = await fetch(urlproductos+ `/${id}`)
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
+export const crearProducto = async (productoNuevo) => {
+    try{
+        const respuesta = await fetch(urlproductos,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productoNuevo)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
+export const editarProducto = async (productoEditado, id) => {
+    try{
+        const respuesta = await fetch(urlproductos+ `/${id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productoEditado)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+export const borrarProductoPorID = async (id) => {
+    try{
+        const respuesta = await fetch(urlproductos+ `/${id}`,{
+            method: 'DELETE',
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
