01ffd0835d772b8241ebcbc992ccb30b4d3f6d23
Emilse Arias <emilsearias@gmail.com>
2025-08-27 19:57:57 -0300

    mostramos el detalle del producto seleccionado en la pagina principal

--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -21,7 +21,7 @@ const CardProducto = ({producto}) => {
           </Card.Text>
         </Card.Body>
         <Card.Footer className="text-end">
-          <Link variant="success" className="me-2 btn btn-success" to={'/detalle/'+producto.id}>
+          <Link variant="success" className="me-2 btn btn-success" to={'/detalle/'+producto._id}>
             Ver más
           </Link>
         </Card.Footer>
