d2a05e6bfc29e09c28b078c1b4d19cfad3501a8e
Emilse Arias <emilsearias@gmail.com>
2025-09-10 21:11:26 -0300

    modificamos la pregunta del componente ProtectorAdmin para verificar si el usuario administrador esta logueado o no

--- src/components/routes/ProtectorAdmin.jsx
+++ src/components/routes/ProtectorAdmin.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router";
 
 const ProtectorAdmin = ({isAdmin}) => {
     //si no soy administrador
-    if(!isAdmin){
+    if(!isAdmin.token){
         return <Navigate to={'/'}/>
     }
     //si soy administrador muestro las rutas
