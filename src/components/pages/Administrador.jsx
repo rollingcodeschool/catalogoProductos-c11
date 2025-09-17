c58c6d9612a46b1024f22fb08a92ca860c50ed39
Emilse Arias <emilsearias@gmail.com>
2025-09-17 19:48:28 -0300

    agregamos un maquetado de paginacion al final de la tabla del administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,4 +1,4 @@
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
 import { Link } from "react-router";
 import { useEffect, useState } from "react";
@@ -7,27 +7,26 @@ import { leerProductos } from "../../helpers/queries";
 const Administrador = () => {
   const [listaProductos, setListaProductos] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     obtenerProductos();
-  },[])
-  
-  const obtenerProductos = async ()=>{
-    const respuesta = await leerProductos()
-    if(respuesta.status === 200){
-      const datos = await respuesta.json()
-      setListaProductos(datos)
-    }else{
-      console.info('Ocurrio un error al buscar los productos')
-    }
-  }
+  }, []);
 
+  const obtenerProductos = async () => {
+    const respuesta = await leerProductos();
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      setListaProductos(datos);
+    } else {
+      console.info("Ocurrio un error al buscar los productos");
+    }
+  };
 
   return (
     <section className="container mainSection">
       <div className="d-flex justify-content-between align-items-center mt-5">
         <h1 className="display-4 ">Productos disponibles</h1>
         <div>
-          <Link className="btn btn-primary" to={'/administrador/crear'}>
+          <Link className="btn btn-primary" to={"/administrador/crear"}>
             <i className="bi bi-file-earmark-plus"></i>
           </Link>
         </div>
@@ -45,11 +44,21 @@ const Administrador = () => {
           </tr>
         </thead>
         <tbody>
-          {
-            listaProductos.map((producto, indice)=><ItemProducto key={producto._id} producto={producto} fila={indice + 1}  setListaProductos={setListaProductos}></ItemProducto>)
-          }
+          {listaProductos.map((producto, indice) => (
+            <ItemProducto
+              key={producto._id}
+              producto={producto}
+              fila={indice + 1}
+              setListaProductos={setListaProductos}
+            ></ItemProducto>
+          ))}
         </tbody>
       </Table>
+      <div className="d-flex justify-content-center align-items-center my-3">
+        <Button variant="secondary">Anterior</Button>
+        <span className="mx-3">Página 1 de 3</span>
+        <Button variant="secondary">Siguiente</Button>
+      </div>
     </section>
   );
 };
